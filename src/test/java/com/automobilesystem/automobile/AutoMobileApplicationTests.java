216935c515d869d7e0d86c735d8857cb131bda9b
ChanukaDasun <chanuka101@gmail.com>
2025-10-01 23:50:43 +0530

    initialize the working dir

---
+++ src/test/java/com/automobilesystem/automobile/AutoMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.automobilesystem.automobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMobileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
