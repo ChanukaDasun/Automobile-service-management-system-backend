0de9e395945942bfb02d1950d4a0576dadf33158
ChanukaDasun <cdasun101@gmail.com>
2025-11-01 14:04:37 +0530

    Add swagger into the Project

--- src/main/java/com/automobilesystem/automobile/AutoMobileApplication.java
+++ src/main/java/com/automobilesystem/automobile/AutoMobileApplication.java
@@ -1,5 +1,5 @@
 package com.automobilesystem.automobile;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AutoMobileApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("MONGO_URI", dotenv.get("MONGO_URI"));
+        System.setProperty("PORT", dotenv.get("PORT"));
         SpringApplication.run(AutoMobileApplication.class, args);
     }
 
--- src/main/java/com/automobilesystem/automobile/controller/TestController.java
+++ src/main/java/com/automobilesystem/automobile/controller/TestController.java
@@ -1,6 +1,4 @@
-package com.automobilesystem.automobile;
-
-
+package com.automobilesystem.automobile.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +9,6 @@ public class TestController {
 
 @GetMapping
     public String test(){
-    return "test";
-}
+        return "test";
+    }
 }
